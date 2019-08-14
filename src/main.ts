789fcf3ad59a182635b05aac62e45c3ad87fe29f
Dietrich Epp <depp@zdome.net>
2019-08-14 12:42:06 -0400

    Create WebGL rendering context

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log('Hello, there.');
+/** Game canvas element. */
+const canvas = document.getElementById('g') as HTMLCanvasElement;
+/** WebGL rendering context. */
+const gl = canvas.getContext('webgl', { alpha: false });
+
+if (!gl) {
+  throw new Error('Could not create WebGL context');
+}
+
+gl.clearColor(0, 0.6, 0.9, 0);
+gl.clear(gl.COLOR_BUFFER_BIT);
