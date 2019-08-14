937af521a0b706d90d25f1bdc47d35ac597e78d2
Dietrich Epp <depp@zdome.net>
2019-08-14 04:38:49 -0400

    Correctly escape JavaScript for embedding in HTML

--- tools/build.ts
+++ tools/build.ts
@@ -1,6 +1,13 @@
 import * as fs from 'fs';
 import * as Handlebars from 'handlebars';
 
+/** Escape JavaScript for embedding in script tag. */
+function inlineJavaScript(js: string): Handlebars.SafeString {
+  return new Handlebars.SafeString(
+    js.replace('</script', '<\\/script').replace('<!--', '<\\!--'),
+  );
+}
+
 async function main() {
   const templateSrc = await fs.promises.readFile('src/index.html', {
     encoding: 'utf8',
@@ -8,7 +15,7 @@ async function main() {
   const template = Handlebars.compile(templateSrc);
   const html = template({
     title: 'Internship at the Apocalypse',
-    script: new Handlebars.SafeString("console.log('dummy text');"),
+    script: inlineJavaScript("console.log('dummy text');"),
   });
   try {
     await fs.promises.mkdir('build');
