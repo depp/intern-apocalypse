20cc4730cf22b8aa265c57159a942f6297674f34
Dietrich Epp <depp@zdome.net>
2019-08-23 17:13:54 -0400

    Add height to level geometry

--- src/render.level.ts
+++ src/render.level.ts
@@ -9,17 +9,18 @@ import { level } from './world';
 import { Random } from './random';
 import { clamp } from './util';
 import { compileShader, compileProgram } from './shader';
+import { Edge } from './level';
 
 const vshader = compileShader(
   gl.VERTEX_SHADER,
   `
-attribute vec2 aPos;
+attribute vec3 aPos;
 attribute vec4 aColor;
 varying vec4 Color;
 uniform mat4 ModelViewProjection;
 void main() {
     Color = aColor;
-    gl_Position = ModelViewProjection * vec4(aPos, 0.0, 1.0);
+    gl_Position = ModelViewProjection * vec4(aPos, 1.0);
 }
   `,
 );
@@ -64,6 +65,19 @@ function concatArrays<T extends TypedArray>(
   return r;
 }
 
+const random = new Random(9876);
+
+function flatColor(n: number, walkable: boolean): Uint8Array {
+  const array = new Uint8Array(n * 4);
+  const luminance =
+    random.range(0.2, 0.4) + ((walkable as unknown) as number) * 0.6;
+  const val = clamp((luminance * 256) | 0, 0, 255);
+  for (let i = 0; i < n; i++) {
+    array.set([val, val, val, 255], i * 4);
+  }
+  return array;
+}
+
 /**
  * Create the level geometry.
  */
@@ -71,21 +85,18 @@ function createGeometry(): void {
   const cellIndexList: Uint16Array[] = [];
   const cellPosList: Float32Array[] = [];
   const cellColorList: Uint8Array[] = [];
-  const random = new Random(9876);
   let index = 0;
+  const wallHeight = 0.7;
   for (const cell of level.cells.values()) {
     if (cell.index < 0) {
       // Border cell.
       continue;
     }
-    const vertexes: Readonly<Vector>[] = [];
-    for (const edge of cell.edges()) {
-      vertexes.push(edge.vertex0);
-    }
-    const n = vertexes.length;
+    const z = ((!cell.walkable as unknown) as number) * wallHeight;
+    const edges: Readonly<Edge>[] = Array.from(cell.edges());
+    const n = edges.length;
     const cellIndex = new Uint16Array((n - 2) * 3);
-    const cellPos = new Float32Array(n * 2);
-    const cellColor = new Uint8Array(n * 4);
+    const cellPos = new Float32Array(n * 3);
     for (let i = 0; i < n - 2; i++) {
       cellIndex[i * 3] = index + 0;
       cellIndex[i * 3 + 1] = index + i + 1;
@@ -93,18 +104,50 @@ function createGeometry(): void {
     }
     index += n;
     for (let i = 0; i < n; i++) {
-      cellPos[i * 2] = vertexes[i].x;
-      cellPos[i * 2 + 1] = vertexes[i].y;
-    }
-    const luminance =
-      random.range(0.2, 0.4) + ((cell.walkable as unknown) as number) * 0.6;
-    const val = clamp((luminance * 256) | 0, 0, 255);
-    for (let i = 0; i < n; i++) {
-      cellColor.set([val, val, val, 255], i * 4);
+      const { x, y } = edges[i].vertex0;
+      cellPos[i * 3] = x;
+      cellPos[i * 3 + 1] = y;
+      cellPos[i * 3 + 2] = z;
     }
     cellIndexList.push(cellIndex);
     cellPosList.push(cellPos);
-    cellColorList.push(cellColor);
+    cellColorList.push(flatColor(n, cell.walkable));
+    if (!cell.walkable) {
+      for (const edge of edges) {
+        const back = level.edgeBack(edge);
+        if (back.cell!.walkable) {
+          const { vertex0, vertex1 } = edge;
+          cellIndexList.push(
+            new Uint16Array([
+              index + 0,
+              index + 1,
+              index + 2,
+              index + 2,
+              index + 1,
+              index + 3,
+            ]),
+          );
+          index += 4;
+          cellPosList.push(
+            new Float32Array([
+              vertex0.x,
+              vertex0.y,
+              0,
+              vertex1.x,
+              vertex1.y,
+              0,
+              vertex0.x,
+              vertex0.y,
+              wallHeight,
+              vertex1.x,
+              vertex1.y,
+              wallHeight,
+            ]),
+          );
+          cellColorList.push(flatColor(4, cell.walkable));
+        }
+      }
+    }
   }
   const indexData = concatArrays(Uint16Array, cellIndexList);
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuf);
@@ -136,7 +179,7 @@ export function renderLevel(): void {
 
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuf);
   gl.bindBuffer(gl.ARRAY_BUFFER, posBuf);
-  gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0);
+  gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
   gl.bindBuffer(gl.ARRAY_BUFFER, colorBuf);
   gl.vertexAttribPointer(1, 4, gl.UNSIGNED_BYTE, true, 0, 0);
 
