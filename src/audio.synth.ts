1f559fc6f7d7d967ee036a7c3367c49ca67a411f
Dietrich Epp <depp@zdome.net>
2019-08-24 08:15:44 -0400

    Implement low-pass filter

--- src/audio.synth.ts
+++ src/audio.synth.ts
@@ -18,6 +18,27 @@ function oscillator(result: Float32Array, pitch: Float32Array): void {
 }
 
 /**
+ * Apply a two-pole low pass filter. Only works up to about 8 kHz.
+ * @param out Output audio.
+ * @param input Input audio.
+ * @param frequency Input frequency.
+ */
+function twoPoleLowPass(
+  out: Float32Array,
+  input: Float32Array,
+  frequency: Float32Array,
+): void {
+  let a = 0;
+  let b = 0;
+  let q = 0.2; // Actually 1 / q.
+  for (let i = 0; i < out.length; i++) {
+    let f = ((2 * Math.PI) / sampleRate) * frequency[i];
+    out[i] = b += f * a;
+    a += f * (input[i] - b - q * a);
+  }
+}
+
+/**
  * Generate envelope data.
  * @param result Array to store output.
  * @param params Envelope time and level parameters: (v0, t1, v1, ...)
@@ -63,11 +84,14 @@ export function generateAudio(): Float32Array {
   const pitch = 440;
   const pitchArray = new Float32Array(length);
   const oscArray = new Float32Array(length);
+  const freqArray = new Float32Array(length);
   const ampArray = new Float32Array(length);
   const outArray = new Float32Array(length);
   pitchArray.fill(pitch / sampleRate);
   oscillator(oscArray, pitchArray);
+  envelope(freqArray, [6000, 1, 1000]);
+  twoPoleLowPass(outArray, oscArray, freqArray);
   envelope(ampArray, [0, 0.1, 1, 0.9, 0]);
-  multiply(outArray, ampArray, oscArray);
+  multiply(outArray, ampArray, outArray);
   return outArray;
 }
