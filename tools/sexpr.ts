6790a165d79f7622899422ed9ecd5b1f1768390b
Dietrich Epp <depp@zdome.net>
2019-08-25 16:15:21 -0400

    Extract printError into a new module

--- tools/sexpr.ts
+++ tools/sexpr.ts
@@ -6,49 +6,11 @@
 
 import * as fs from 'fs';
 
-import chalk from 'chalk';
-
 import { readStream } from './stream';
 
 import { SExpr, parseSExpr, printSExpr } from '../src/sexpr';
-import { SourceSpan, SourceText, SourceError } from '../src/sourcepos';
-
-const colorFilename: (s: string) => string = chalk.cyan;
-const colorLoc: (s: string) => string = chalk.yellow;
-const colorError: (s: string) => string = chalk.red;
-const colorErrorUnderline: (s: string) => string = chalk.red;
-const colorLineNumber: (s: string) => string = chalk.inverse;
-
-/**
- * Print an error message.
- */
-function showError(text: SourceText, e: Error & SourceSpan): void {
-  const start = text.lookup(e.sourceStart);
-  const end = text.lookup(e.sourceEnd);
-  process.stderr.write(
-    `${colorFilename(text.name)}:` +
-      `${colorLoc(start.lineno.toString())}:` +
-      `${colorLoc(start.colno.toString())} - ` +
-      `${colorError('error')}: ` +
-      e.message +
-      '\n',
-  );
-  const numLen = end.lineno.toString().length;
-  for (let lineno = start.lineno; lineno <= end.lineno; lineno++) {
-    const line = text.lines[lineno - 1];
-    const startChar = lineno > start.lineno ? 0 : start.colno - 1;
-    let endChar = lineno < end.lineno ? line.length : end.colno - 1;
-    endChar = Math.max(Math.min(endChar, line.length), startChar + 1);
-    process.stderr.write(
-      `${colorLineNumber(lineno.toString().padStart(numLen))} ${line}\n`,
-    );
-    process.stderr.write(
-      ' '.repeat(numLen + 1 + startChar) +
-        colorErrorUnderline('~'.repeat(endChar - startChar)) +
-        '\n',
-    );
-  }
-}
+import { SourceText, SourceError } from '../src/sourcepos';
+import { printError } from './source';
 
 /**
  * Process input containing S-expression code.
@@ -61,7 +23,7 @@ function processInput(name: string, source: string): boolean {
   } catch (e) {
     if (e instanceof SourceError) {
       const text = new SourceText(name, source);
-      showError(text, e);
+      printError(text, e);
       return false;
     }
     throw e;
