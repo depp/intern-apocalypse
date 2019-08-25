7af59ff43a297a94f64c04de51193d51e3ee7874
Dietrich Epp <depp@zdome.net>
2019-08-25 00:46:17 -0400

    Add S-expression parser

--- src/sexpr.test.ts
+++ src/sexpr.test.ts
@@ -1,4 +1,14 @@
-import { TokenType, Token, tokenize } from './sexpr';
+import {
+  TokenType,
+  Token,
+  tokenize,
+  parseSExpr,
+  SExprType,
+  SExpr,
+  SymbolExpr,
+  printSExpr,
+  ListExpr,
+} from './sexpr';
 
 function formatToken(tok: Token): string {
   return `(${TokenType[tok.type]},${JSON.stringify(tok.text)},${
@@ -13,6 +23,7 @@ test('tokenize', () => {
     { type: TokenType.Symbol, text: 'abc', sourcePos: 1 },
     { type: TokenType.CloseParen, text: ')', sourcePos: 4 },
     { type: TokenType.Symbol, text: 'def', sourcePos: 16 },
+    { type: TokenType.End, text: '', sourcePos: 19 },
   ];
   if (tokens.length != expect.length) {
     throw new Error(`got ${tokens.length} tokens, expect ${expect.length}`);
@@ -30,4 +41,56 @@ test('tokenize', () => {
       );
     }
   }
+});
+
+test('parse', () => {
+  const text = 'abc (def ghi) (jkl (() 1 2 3))';
+  const nodes = parseSExpr(text);
+  const root: ListExpr = {
+    type: SExprType.List,
+    sourceStart: 0,
+    sourceEnd: text.length,
+    items: nodes,
+  };
+  const expect: any = ['abc', ['def', 'ghi'], ['jkl', [[], '1', '2', '3']]];
+  function compare(x: SExpr, y: any): void {
+    switch (x.type) {
+      case SExprType.Symbol:
+        const sym = x as SymbolExpr;
+        if (typeof y != 'string') {
+          throw new Error(
+            `invalid type: got ${printSExpr(x)}, expected ${JSON.stringify(y)}`,
+          );
+        }
+        if (sym.name != y) {
+          throw new Error(
+            `incorrect name: got ${JSON.stringify(
+              sym.name,
+            )}, expected ${JSON.stringify(y)}`,
+          );
+        }
+        break;
+      case SExprType.List:
+        const list = x as ListExpr;
+        if (typeof y != 'object') {
+          throw new Error(
+            `invalid type: got ${printSExpr(x)}, expected ${JSON.stringify(y)}`,
+          );
+        }
+        if (list.items.length != y.length) {
+          throw new Error(
+            `invalid list length: got ${printSExpr(
+              x,
+            )}, expected ${JSON.stringify(y)}`,
+          );
+        }
+        for (let i = 0; i < y.length; i++) {
+          compare(list.items[i], y[i]);
+        }
+        break;
+      default:
+        throw new Error(`unknown type ${x.type}`);
+    }
+  }
+  compare(root, expect);
 });
--- src/sexpr.ts
+++ src/sexpr.ts
@@ -1,3 +1,6 @@
+import { AssertionError } from './debug';
+
+/** Lexical token types. */
 export enum TokenType {
   End,
   Error,
@@ -8,6 +11,7 @@ export enum TokenType {
   CloseParen,
 }
 
+/** Lexical token. */
 export interface Token {
   readonly type: TokenType;
   readonly text: string;
@@ -39,7 +43,7 @@ const tokenTypes: readonly TokenType[] = (() => {
   return result;
 })();
 
-/** Split a string into a list of tokens. */
+/** Split a string into a list of lexical tokens. */
 export function tokenize(source: string): Token[] {
   const tokens: Token[] = [];
   const matchToken = /[-!$%&*+./:<=>?@^_~0-9a-zA-Z]+|[()]|[ \n\r\t]+|;.*?$/my;
@@ -56,5 +60,97 @@ export function tokenize(source: string): Token[] {
         break;
     }
   }
+  tokens.push({ type: TokenType.End, text: '', sourcePos: source.length });
   return tokens;
+}
+
+/** S-expression node type. */
+export enum SExprType {
+  Symbol,
+  List,
+}
+
+/** S-expression node base. */
+export interface SExpr {
+  readonly type: SExprType;
+  readonly sourceStart: number;
+  readonly sourceEnd: number;
+}
+
+/** S-expression symbol. */
+export interface SymbolExpr extends SExpr {
+  readonly name: string;
+}
+
+/** S-expression list. */
+export interface ListExpr extends SExpr {
+  readonly items: readonly SExpr[];
+}
+
+/** An error parsing an S-expression. */
+export class SyntaxError extends Error {
+  sourcePos: number;
+  constructor(message: string, sourcePos: number) {
+    super(message);
+    this.sourcePos = sourcePos;
+  }
+}
+
+/** Parse a string into a list of top-level S-expressions. */
+export function parseSExpr(source: string): SExpr[] {
+  const tokens = tokenize(source);
+  let tokenPos = 0;
+  function parseNode(list: SExpr[]): boolean {
+    const start = tokens[tokenPos];
+    switch (start.type) {
+      case TokenType.Symbol:
+        tokenPos++;
+        list.push({
+          type: SExprType.Symbol,
+          sourceStart: start.sourcePos,
+          sourceEnd: start.sourcePos + start.text.length,
+          name: start.text,
+        } as SymbolExpr);
+        return true;
+      case TokenType.OpenParen:
+        tokenPos++;
+        const items: SExpr[] = [];
+        while (parseNode(items)) {}
+        const end = tokens[tokenPos];
+        if (end.type != TokenType.CloseParen) {
+          throw new SyntaxError('unclosed "("', start.sourcePos);
+        }
+        tokenPos++;
+        list.push({
+          type: SExprType.List,
+          sourceStart: start.sourcePos,
+          sourceEnd: end.sourcePos,
+          items,
+        } as ListExpr);
+        return true;
+      default:
+        return false;
+    }
+  }
+  const toplevel: SExpr[] = [];
+  while (parseNode(toplevel)) {}
+  const tok = tokens[tokenPos];
+  if (tok.type != TokenType.End) {
+    throw new SyntaxError('unexpected token', tok.sourcePos);
+  }
+  return toplevel;
+}
+
+/** Print an S-expression as a string. */
+export function printSExpr(expr: SExpr): string {
+  switch (expr.type) {
+    case SExprType.Symbol:
+      const sym = expr as SymbolExpr;
+      return sym.name;
+    case SExprType.List:
+      const list = expr as ListExpr;
+      return `(${list.items.map(printSExpr).join(' ')})`;
+    default:
+      throw new AssertionError('unknown expr type');
+  }
 }
