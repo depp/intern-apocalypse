052f5ee0fb73585356d06c0ab3f1b60ed3bc23dc
Dietrich Epp <depp@zdome.net>
2019-08-27 01:32:59 -0400

    Generate dynamic shader loading info

--- tools/shader.emit.ts
+++ tools/shader.emit.ts
@@ -9,10 +9,15 @@ function titleCase(s: string): string {
   return s.charAt(0).toUpperCase() + s.substring(1);
 }
 
-function programStub(
+interface Output {
+  stub: string;
+  loader: string;
+}
+
+function programLoader(
   program: Program,
   decls: ReadonlyMap<string, Declarations>,
-): string {
+): Output {
   const vdecls = decls.get(program.vertex);
   if (vdecls == null) {
     throw new Error(`missing vertex decls: ${program.vertex}`);
@@ -25,13 +30,23 @@ function programStub(
   const uniforms = Array.from(new Set(vdecls.uniforms.concat(fdecls.uniforms)));
   uniforms.sort();
 
-  let out = `export interface ${tname} extends ShaderProgram {\n`;
+  let stub = `export interface ${tname} extends ShaderProgram {\n`;
   for (const name of uniforms) {
-    out += `  ${name}: WebGLUniformLocation | null;\n`;
+    stub += `  ${name}: WebGLUniformLocation | null;\n`;
   }
-  out += '}\n';
-  out += `export const ${program.name} = compileProgram() as ${tname};\n`;
-  return out;
+  stub += '}\n';
+  stub += `export const ${program.name} = compileProgram() as ${tname};\n`;
+
+  let loader = '{\n';
+  loader += `  name: ${JSON.stringify(program.name)},\n`;
+  loader += `  vertex: ${JSON.stringify(program.vertex)},\n`;
+  loader += `  fragment: ${JSON.stringify(program.fragment)},\n`;
+  loader += `  attributes: ${JSON.stringify(program.attributes)},\n`;
+  loader += `  uniforms: ${JSON.stringify(uniforms)},\n`;
+  loader += `  object: ${program.name},\n`;
+  loader += '},\n';
+
+  return { stub, loader };
 }
 
 /**
@@ -45,12 +60,35 @@ export function emitLoader(
   for (const [name, ast] of code.entries()) {
     decls.set(name, listDeclarations(ast));
   }
+
+  let stubs = '';
+  let loaders = '';
+  for (const program of programs) {
+    const { stub, loader } = programLoader(program, decls);
+    if (stubs != '') {
+      stubs += '\n';
+    }
+    stubs += stub;
+    loaders += loader;
+  }
+
   let out =
     '/* This code is automatically generated. */\n' +
-    "import { compileShader, ShaderProgram } from './shader';\n";
-  for (const program of programs) {
-    out += '\n';
-    out += programStub(program, decls);
+    "import { compileShader, ShaderProgram, ShaderSpec } from './shader';\n" +
+    '\n';
+  out += stubs;
+  out += '\n';
+  out += 'export function getShaderSpecs(): ShaderSpec[] {\n';
+  out += '  return [\n';
+  for (const line of loaders.split('\n')) {
+    if (line != '') {
+      out += '    ';
+      out += line;
+      out += '\n';
+    }
   }
+  out += '  ];\n';
+  out += '}\n';
+
   return out;
 }
