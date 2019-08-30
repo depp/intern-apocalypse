96894ff7115b9d40168197b397d33ec183f92bd1
Dietrich Epp <depp@zdome.net>
2019-08-30 04:12:40 -0400

    Add library for working with units

---
+++ src/synth/units.test.ts
@@ -0,0 +1,29 @@
+import { Units, multiplyUnits } from './units';
+
+test('multiply', () => {
+  interface Case {
+    units: Units[];
+    result: Units;
+  }
+  const cases: Case[] = [{ units: [], result: Units.None }];
+  for (const key of Object.keys(Units)) {
+    const value = Units[key as any];
+    if (typeof value == 'number') {
+      const units = value as Units;
+      cases.push({ units: [value], result: value });
+    }
+  }
+  cases.push(
+    { units: [Units.None, Units.Second, Units.None], result: Units.Second },
+    { units: [Units.Second, Units.Hertz], result: Units.None },
+  );
+  for (const tcase of cases) {
+    const result = multiplyUnits(tcase.units);
+    if (result != tcase.result) {
+      const input = `[${tcase.units.map(x => Units[x]).join(', ')}]`;
+      const output = Units[result];
+      const expect = Units[tcase.result];
+      throw new Error(`multiply ${input}: got ${output}, expect ${expect}`);
+    }
+  }
+});
---
+++ src/synth/units.ts
@@ -0,0 +1,75 @@
+import { AssertionError } from '../debug';
+
+/** Units that expressions can have. */
+export enum Units {
+  /** Unitless number, used as a ratio. */
+  None,
+  /** Audio output, in Volts, with full scale = 1V. */
+  Volt,
+  /** Frequency, in Hertz. */
+  Hertz,
+  /** Time, in seconds. */
+  Second,
+  /** Oscillator phase, where a full cycle has a value of 1. */
+  Phase,
+}
+
+/** Fundamental components of units, as multiplied base units with exponents. */
+interface UnitComponents {
+  second: number;
+  volt: number;
+  phase: number;
+}
+
+const ratioComponents: Readonly<UnitComponents> = {
+  second: 0,
+  volt: 0,
+  phase: 0,
+};
+
+/** Map from units to their base components. */
+const unitComponents: ReadonlyMap<Units, Readonly<UnitComponents>> = (() => {
+  type PartialComponents = { [K in keyof UnitComponents]?: number };
+  const items: [Units, PartialComponents][] = [
+    [Units.None, {}],
+    [Units.Volt, { volt: 1 }],
+    [Units.Hertz, { second: -1 }],
+    [Units.Second, { second: 1 }],
+    [Units.Phase, { phase: 1 }],
+  ];
+  const m = new Map<Units, Readonly<UnitComponents>>();
+  for (const [units, partial] of items) {
+    m.set(units, Object.assign({}, ratioComponents, partial));
+  }
+  return m;
+})();
+
+export class UnitError extends Error {}
+
+export function multiplyUnits(units: Units[]): Units {
+  let c: UnitComponents = Object.assign({}, ratioComponents);
+  for (const unit of units) {
+    const cm = unitComponents.get(unit);
+    if (cm == null) {
+      throw new AssertionError(`missing definition for unit ${Units[unit]}`);
+    }
+    for (const key in c) {
+      const k = key as keyof UnitComponents;
+      c[k] += cm[k];
+    }
+  }
+  for (const [uresult, cr] of unitComponents.entries()) {
+    let match = true;
+    for (const key in c) {
+      const k = key as keyof UnitComponents;
+      if (c[k] != cr[k]) {
+        match = false;
+        break;
+      }
+    }
+    if (match) {
+      return uresult;
+    }
+  }
+  throw new UnitError(`no units match ${JSON.stringify(c)}`);
+}
