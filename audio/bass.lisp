893204c8d0a9fdbaa9589a18aa0a5194ed10f1f1
Dietrich Epp <depp@zdome.net>
2019-09-11 14:03:59 -0400

    Tweak bass sound

--- audio/bass.lisp
+++ audio/bass.lisp
@@ -1,12 +1,16 @@
 ;; Base oscillator
 (define osc (oscillator (note -24)))
 
+(define fenv (frequency (envelope (set 0.6) (exp 70ms -0.2))))
+
 ;; Simple filter sweep.
-(* (lowPass2
-    (mix
-     -9dB (sawtooth (overtone 2 osc))
-     -6dB (sawtooth osc))
-    (frequency (envelope (set 0.2) (lin 1000ms -0.2)))
-    1.6)
-   (envelope (exp 0.5ms 1) (lin 50ms 0.5) (delay 600ms)
-	     (gate) (exp 100ms 0) (stop)))
+(highPass
+ 30Hz
+ (saturate
+  (* (lowPass4
+      (mix
+       -9dB (sawtooth (overtone 2 osc))
+       -6dB (sawtooth osc))
+      fenv 1.5)
+     (envelope (lin 0.05ms 1) (exp 100ms 0.2) (delay 600ms)
+	       (gate) (exp 50ms 0) (stop)))))
